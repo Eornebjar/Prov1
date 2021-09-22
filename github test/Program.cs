73c941f655df3b947a08fabe2eb69cfb5b67c114
Eornebjar <90768180+Eornebjar@users.noreply.github.com>
2021-09-22 11:06:54 +0200

    Add files via upload

---
+++ github test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace github_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NameChecking namnobj = new NameChecking();
+            ProcessingData boolclass = new ProcessingData();
+
+            klasser.nummer1();
+            Console.ReadKey();
+            klasser.nummer2();
+            Console.ReadKey();
+
+            
+            namnobj.namn1();
+            namnobj.namn2();
+
+            boolclass.checks();
+        }
+    }
+}
---
+++ github test/klasser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace github_test
+{
+    class klasser
+    {
+        public static void nummer1()
+        {
+            int iNummer;
+            Console.WriteLine("skriv in ett tal");
+           iNummer = int.Parse(Console.ReadLine());
+
+        }
+
+        public static void nummer2()
+        {
+            int iNummer2;
+            Console.WriteLine("skriv ett till tal");
+            iNummer2 = int.Parse(Console.ReadLine());
+        }
+    }
+    
+    class NameChecking
+    {
+        public void namn1() 
+        {
+            string sFnamn;
+            Console.WriteLine("skriv in ditt förnamn");
+            sFnamn = Console.ReadLine();
+
+        }
+
+        public void namn2()
+        {
+            string sEnamn;
+            Console.WriteLine("skriv in ditt förnamn");
+            sEnamn = Console.ReadLine();
+        }
+    }
+
+    class ProcessingData
+    {
+        public void checks()
+        {
+            bool check = true;
+
+            if (check)
+            {
+                Console.WriteLine(check);
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
+
+
